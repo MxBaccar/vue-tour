097aeae6fdfa18409a5db07f00bce0d1a1d982b1
mourad baccar <mourad.baccar@digitrends.dev>
2022-08-19 16:36:15 +0200

    pass stringified function from vuex

--- src/components/VTour.vue
+++ src/components/VTour.vue
@@ -43,203 +43,215 @@
 </template>
 
 <script>
-import { DEFAULT_CALLBACKS, DEFAULT_OPTIONS, KEYS } from '../shared/constants'
+import { DEFAULT_CALLBACKS, DEFAULT_OPTIONS, KEYS } from "../shared/constants";
 
 export default {
-  name: 'v-tour',
+  name: "v-tour",
   props: {
     steps: {
       type: Array,
-      default: () => []
+      default: () => [],
     },
     name: {
-      type: String
+      type: String,
     },
     options: {
       type: Object,
-      default: () => { return DEFAULT_OPTIONS }
+      default: () => {
+        return DEFAULT_OPTIONS;
+      },
     },
     callbacks: {
       type: Object,
-      default: () => { return DEFAULT_CALLBACKS }
-    }
+      default: () => {
+        return DEFAULT_CALLBACKS;
+      },
+    },
   },
-  data () {
+  data() {
     return {
-      currentStep: -1
-    }
+      currentStep: -1,
+    };
   },
-  mounted () {
-    this.$tours[this.name] = this
+  mounted() {
+    this.$tours[this.name] = this;
   },
-  beforeDestroy () {
+  beforeDestroy() {
     // Remove the keyup listener if it has been defined
     if (this.customOptions.useKeyboardNavigation) {
-      window.removeEventListener('keyup', this.handleKeyup)
+      window.removeEventListener("keyup", this.handleKeyup);
     }
   },
   computed: {
     // Allow us to define custom options and merge them with the default options.
     // Since options is a computed property, it is reactive and can be updated during runtime.
-    customOptions () {
+    customOptions() {
       return {
         ...DEFAULT_OPTIONS,
-        ...this.options
-      }
+        ...this.options,
+      };
     },
-    customCallbacks () {
+    customCallbacks() {
       return {
         ...DEFAULT_CALLBACKS,
-        ...this.callbacks
-      }
+        ...this.callbacks,
+      };
     },
     // Return true if the tour is active, which means that there's a VStep displayed
-    isRunning () {
-      return this.currentStep > -1 && this.currentStep < this.numberOfSteps
+    isRunning() {
+      return this.currentStep > -1 && this.currentStep < this.numberOfSteps;
     },
-    isFirst () {
-      return this.currentStep === 0
+    isFirst() {
+      return this.currentStep === 0;
     },
-    isLast () {
-      return this.currentStep === this.steps.length - 1
+    isLast() {
+      return this.currentStep === this.steps.length - 1;
     },
-    numberOfSteps () {
-      return this.steps.length
+    numberOfSteps() {
+      return this.steps.length;
     },
-    step () {
-      return this.steps[this.currentStep]
-    }
+    step() {
+      return this.steps[this.currentStep];
+    },
   },
   methods: {
-    async start (startStep) {
+    async start(startStep) {
       // Register keyup listeners for this tour
       if (this.customOptions.useKeyboardNavigation) {
-        window.addEventListener('keyup', this.handleKeyup)
+        window.addEventListener("keyup", this.handleKeyup);
       }
 
       // Wait for the DOM to be loaded, then start the tour
-      startStep = typeof startStep !== 'undefined' ? parseInt(startStep, 10) : 0
-      let step = this.steps[startStep]
+      startStep =
+        typeof startStep !== "undefined" ? parseInt(startStep, 10) : 0;
+      let step = this.steps[startStep];
 
-      let process = () => new Promise((resolve, reject) => {
-        setTimeout(() => {
-          this.customCallbacks.onStart()
-          this.currentStep = startStep
-          resolve()
-        }, this.customOptions.startTimeout)
-      })
+      let process = () =>
+        new Promise((resolve, reject) => {
+          setTimeout(() => {
+            this.customCallbacks.onStart();
+            this.currentStep = startStep;
+            resolve();
+          }, this.customOptions.startTimeout);
+        });
 
-      if (typeof step.before !== 'undefined') {
+      if (typeof step.before !== "undefined") {
         try {
-          await step.before('start')
+          await step.before("start");
         } catch (e) {
-          return Promise.reject(e)
+          return Promise.reject(e);
         }
       }
-      await process()
+      await process();
 
-      return Promise.resolve()
+      return Promise.resolve();
     },
-    async previousStep () {
-      let futureStep = this.currentStep - 1
+    async previousStep() {
+      let futureStep = this.currentStep - 1;
 
-      let process = () => new Promise((resolve, reject) => {
-        this.customCallbacks.onPreviousStep(this.currentStep)
-        this.currentStep = futureStep
-        resolve()
-      })
+      let process = () =>
+        new Promise((resolve, reject) => {
+          this.customCallbacks.onPreviousStep(this.currentStep);
+          this.currentStep = futureStep;
+          resolve();
+        });
 
       if (futureStep > -1) {
-        let step = this.steps[futureStep]
-        if (typeof step.before !== 'undefined') {
+        let step = this.steps[futureStep];
+        if (typeof step.before !== "undefined") {
           try {
-            await step.before('previous')
+            await step.before("previous");
           } catch (e) {
-            return Promise.reject(e)
+            return Promise.reject(e);
           }
         }
-        await process()
+        await process();
       }
 
-      return Promise.resolve()
+      return Promise.resolve();
     },
-    async nextStep () {
-      let futureStep = this.currentStep + 1
+    async nextStep() {
+      let futureStep = this.currentStep + 1;
 
-      let process = () => new Promise((resolve, reject) => {
-        this.customCallbacks.onNextStep(this.currentStep)
-        this.currentStep = futureStep
-        resolve()
-      })
+      let process = () =>
+        new Promise((resolve, reject) => {
+          this.customCallbacks.onNextStep(this.currentStep);
+          this.currentStep = futureStep;
+          resolve();
+        });
 
       if (futureStep < this.numberOfSteps && this.currentStep !== -1) {
-        let step = this.steps[futureStep]
-        if (typeof step.before !== 'undefined') {
+        let step = this.steps[futureStep];
+        if (typeof step.before !== "undefined") {
           try {
-            await step.before('next')
+            if (typeof step.before == "string")
+              step.before = new Function("return " + step.before)();
+            await step.before("next");
           } catch (e) {
-            return Promise.reject(e)
+            return Promise.reject(e);
           }
         }
-        await process()
+        await process();
       }
 
-      return Promise.resolve()
+      return Promise.resolve();
     },
-    stop () {
-      this.customCallbacks.onStop()
-      document.body.classList.remove('v-tour--active')
-      this.currentStep = -1
+    stop() {
+      this.customCallbacks.onStop();
+      document.body.classList.remove("v-tour--active");
+      this.currentStep = -1;
     },
-    skip () {
-      this.customCallbacks.onSkip()
-      this.stop()
+    skip() {
+      this.customCallbacks.onSkip();
+      this.stop();
     },
-    finish () {
-      this.customCallbacks.onFinish()
-      this.stop()
+    finish() {
+      this.customCallbacks.onFinish();
+      this.stop();
     },
 
-    handleKeyup (e) {
+    handleKeyup(e) {
       if (this.customOptions.debug) {
-        console.log('[Vue Tour] A keyup event occured:', e)
+        console.log("[Vue Tour] A keyup event occured:", e);
       }
       switch (e.keyCode) {
         case KEYS.ARROW_RIGHT:
-          this.isKeyEnabled('arrowRight') && this.nextStep()
-          break
+          this.isKeyEnabled("arrowRight") && this.nextStep();
+          break;
         case KEYS.ARROW_LEFT:
-          this.isKeyEnabled('arrowLeft') && this.previousStep()
-          break
+          this.isKeyEnabled("arrowLeft") && this.previousStep();
+          break;
         case KEYS.ESCAPE:
-          this.isKeyEnabled('escape') && this.stop()
-          break
+          this.isKeyEnabled("escape") && this.stop();
+          break;
       }
     },
-    isKeyEnabled (key) {
-      const { enabledNavigationKeys } = this.customOptions
-      return enabledNavigationKeys.hasOwnProperty(key) ? enabledNavigationKeys[key] : true
-    }
-  }
-}
+    isKeyEnabled(key) {
+      const { enabledNavigationKeys } = this.customOptions;
+      return enabledNavigationKeys.hasOwnProperty(key)
+        ? enabledNavigationKeys[key]
+        : true;
+    },
+  },
+};
 </script>
 
 <style lang="scss">
-  body.v-tour--active {
-    pointer-events: none;
-  }
+body.v-tour--active {
+  pointer-events: none;
+}
 
-  .v-tour {
-    pointer-events: auto;
-  }
+.v-tour {
+  pointer-events: auto;
+}
 
-  .v-tour__target--highlighted {
-    box-shadow: 0 0 0 4px rgba(0,0,0,.4);
-    pointer-events: auto;
-    z-index: 9999;
-  }
+.v-tour__target--highlighted {
+  box-shadow: 0 0 0 4px rgba(0, 0, 0, 0.4);
+  pointer-events: auto;
+  z-index: 9999;
+}
 
-  .v-tour__target--relative {
-    position: relative;
-  }
+.v-tour__target--relative {
+  position: relative;
+}
 </style>
